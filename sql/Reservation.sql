c66814b39d6466926d2cc0d56db24804b6ca798a
Quentin Millardet <quentin@millardet.be>
2018-02-08 17:56:58 +0100

    Modification de la table Reservation (Ajout id_item)

--- sql/Reservation.sql
+++ sql/Reservation.sql
@@ -9,10 +9,12 @@ CREATE TABLE RESERVATION (
   h_fin int(2),
   j_fin int(2),
   id_utili int(11),
+  id_item int(11),
   etat BOOLEAN,
   creation DATE,
   modification DATE,
   PRIMARY KEY (id_res)
 )ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-ALTER TABLE RESERVATION ADD CONSTRAINT `reserv_idUtil_1` FOREIGN KEY (`id_utili`) REFERENCES `user` (`id`)
+ALTER TABLE RESERVATION ADD CONSTRAINT `reserv_idUtil_1` FOREIGN KEY (`id_utili`) REFERENCES `user` (`id`);
+ALTER TABLE RESERVATION ADD CONSTRAINT `reserv_idItem_1` FOREIGN KEY (`id_item`) REFERENCES `item` (`id`);
