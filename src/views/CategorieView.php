0b7a30c1f7386ef4a44dc97fd2973db7d70e4af1
Loic Oberle <loicoberle@neuf.fr>
2018-02-08 21:09:28 +0100

    modification css page d'accueil

--- src/views/CategorieView.php
+++ src/views/CategorieView.php
@@ -10,34 +10,34 @@ class CategorieView {
  		$this->cat = $c;
 	}
 
- 	public function render() {	
+ 	public function render() {
  		$html = '<div class="ensembleListes">';
  		$app = \Slim\Slim::getInstance();
  		foreach ($this->cat as $c) {
  			$urlListeItemsCat = $app->urlFor('ListeItemsCat', ['id' => $c->id]);
- 			$html = $html.'<a href = "'.$urlListeItemsCat.'" class="liste"><div >';
+ 			$html = $html.'<a id="bouton "href = "'.$urlListeItemsCat.'" class="liste"><button id="bouton"><div >';
 			$html = $html.'<div class="cat_nom">'.$c->nom."</div><div class=\"cat_desc\">".$c->description."</div>";
-			$html = $html.'</div></a>';
+			$html = $html.'</div></button></a>';
  		}
  		$html = $html.'</div>';
 		return $html;
 	}
 
-	public function render_id() {		
+	public function render_id() {
 		$html = '<div class="cat_id">';
 		$html = $html.$this->cat->id."<br>";
 		$html = $html.'</div>';
 		return $html;
 	}
 
-	public function render_nom() {		
+	public function render_nom() {
 		$html = '<div class="cat_nom">';
 		$html = $html.$this->cat->nom."<br>";
 		$html = $html.'</div>';
 		return $html;
 	}
 
-	public function render_desc() {		
+	public function render_desc() {
 		$html = '<div class="cat_desc">';
 		$html = $html.$this->cat->description."<br>";
 		$html = $html.'</div>';
