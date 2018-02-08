dca9d26b2ffced61d99d1c52c996d28c72d78cbb
Aymerik DIEBOLD <aymerik.diebold@free.fr>
2018-02-08 11:36:40 +0100

    Modification BDD

--- sql/ccd2018.sql
+++ sql/ccd2018.sql
@@ -1,6 +1,9 @@
 -- phpMyAdmin SQL Dump
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 08 fév. 2018 à 10:34
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -9,10 +12,11 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `ccd2018`
@@ -94,6 +98,8 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
@@ -101,19 +107,19 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `nom`) VALUES
-(1, 'Cassandre'),
-(2, 'Achille'),
-(3, 'Calypso'),
-(4, 'Bacchus'),
-(5, 'Diane'),
-(6, 'Clark'),
-(7, 'Helene'),
-(8, 'Jason'),
-(9, 'Bruce'),
-(10, 'Pénélope'),
-(11, 'Ariane'),
-(12, 'Lois');
+INSERT INTO `user` (`id`, `nom`, `username`, `password`) VALUES
+(1, 'Cassandre', '', ''),
+(2, 'Achille', '', ''),
+(3, 'Calypso', '', ''),
+(4, 'Bacchus', '', ''),
+(5, 'Diane', '', ''),
+(6, 'Clark', '', ''),
+(7, 'Helene', '', ''),
+(8, 'Jason', '', ''),
+(9, 'Bruce', '', ''),
+(10, 'Pénélope', '', ''),
+(11, 'Ariane', '', ''),
+(12, 'Lois', '', '');
 
 --
 -- Contraintes pour les tables déchargées
