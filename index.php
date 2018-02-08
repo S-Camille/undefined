658c57655024b9a604ff9ddf9fc8ce10ca66f39b
Aymerik DIEBOLD <aymerik.diebold@free.fr>
2018-02-08 10:51:19 +0100

    Config eloquent

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 require 'vendor/autoload.php';
 
+session_start();
+
+$db = new Illuminate\Database\Capsule\Manager();
+$db->addConnection(parse_ini_file('src/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+
 $app = new Slim\Slim();
 
 $app->get('/', function(){
