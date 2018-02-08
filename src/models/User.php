cf25886bc8f322f557618b7d5b6156a5acff0627
Luc Cheng <luc.cheng7@etu.univ-lorraine.fr>
2018-02-08 11:05:18 +0100

    Ajoute la classe User

---
+++ src/models/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luc
+ * Date: 08/02/2018
+ * Time: 11:03
+ */
+
+namespace undefined\models;
+class User extends \Illuminate\Database\Eloquent\Model {
+    protected $table = 'user';
+    protected $primaryKey = 'nom';
+    public $timestamps = false;
+    
+}
