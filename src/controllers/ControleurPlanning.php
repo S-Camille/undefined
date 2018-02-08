b8f65bf7f6d4f8cf7d1eb693379288024dce29ea
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2018-02-08 20:07:12 +0100

    Planning + css

--- src/controllers/ControleurPlanning.php
+++ src/controllers/ControleurPlanning.php
@@ -8,34 +8,33 @@ use undefined\views\VueCategorie;
 use undefined\views\GlobaleView;
 use undefined\models\Reservation;
 
-class ControleurPlanning{
+class ControleurPlanning {
+
   private $reserve;
+ 
+  private $heures = ["8H", "10H", "12H", "14H", "16H", "18H"];
 
-  private $heures=["8H","9H","10H","11H","12H","13H","14H","15H","16H","17H","18H"];
+  private $jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"];
 
-  private $jours=["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
-
-  function afficherPlanning($id){
-      $tab=$this->initialiserPlanning();
-
-      $reservations=Reservation::where('id_item','=',$id)->get();
-
+  function afficherPlanning($id) {
+      $tab = $this->initialiserPlanning();
+      $reservations = Reservation::where('id_item','=',$id)->get();
       foreach ($reservations as $key => $value) {
-        if($value->j_debut<$value->j_fin){
+        if ($value->j_debut<$value->j_fin) {
           for ($j=0; $j < $value->j_fin-1 ; $j++) {
             for ($i=0; $i < 11; $i++) {
-              $tab=$this->reserverHoraire($j,$i,$tab);
+              $tab = $this->reserverHoraire($j,$i,$tab);
             }
           }
         }
         for ($l=0; $l < $value->h_fin-8; $l++) {
-          $tab=$this->reserverHoraire($value->j_fin-1,$l,$tab);
+          $tab = $this->reserverHoraire($value->j_fin-1,$l,$tab);
         }
       }
-      $vP=new VuePlanning($this->affichagePlanning($tab));
+      $vP = new VuePlanning($this->affichagePlanning($tab));
       $vP->render();
   }
-
+  
   function afficherPlanningUtilisateur($id){
     $tab=$this->initialiserPlanning();
     $reservations=Reservation::where('id_utili',"=",$id)->get();
@@ -56,52 +55,44 @@ class ControleurPlanning{
 
   }
 
+  function initialiserPlanning() {
+    $reserve = array();
 
-  function initialiserPlanning(){
-    $reserve=array();
     for ($i=0; $i < 7; $i++) {
-      $t=[];
+      $t = [];
       for ($j=0; $j < 11; $j++) {
-        $t[]="Vacant";
+        $t[] = "Vacant";
       }
-      $reserve[]=$t;
+      $reserve[] = $t;
     }
     return $reserve;
   }
 
-  function reserverHoraire($j,$h,$tab){
-      $reserve=$tab;
-    $reserve[$j][$h]="Reserve";
+  function reserverHoraire($j,$h,$tab) {
+    $reserve = $tab;
+    $reserve[$j][$h] = "Reserve";
     return $reserve;
 
   }
 
-
-  function affichagePlanning($tab){
-    $heures=$this->heures;
-    $jours=$this->jours;
-    $reserve=$tab;
-    $res='<table><tr><td>Horaires</td>';
+  function affichagePlanning($tab) {
+    $heures = $this->heures;
+    $jours = $this->jours;
+    $reserve = $tab;
+    $res = '<table id="resa"><tr><th></th>';
     foreach ($jours as $k => $v) {
-      $res.="<td>".$v."</td>";
+      $res .= "<th>".$v."</th>";
     }
-    $res.="</tr>";
-
-    $res.="<tr>";
+    $res .= "</tr>";
+    $res .= "<tr>";
     foreach ($heures as $k => $v) {
-    $res.="<td>".$heures[$k]."</td>";
+      $res .= "<td>".$heures[$k]."</td>";
       foreach ($jours as $key => $value) {
-      $res.="<td>".$reserve[$key][$k]."</td>";
+        $res .= "<td class=\"".(($reserve[$key][$k] == 'Vacant') ? 'dispo' : 'indispo')."\">".$reserve[$key][$k]."</td>";
+      }
+    $res .= "</tr>";
     }
-    $res.="</tr>";
-  }
-
     return $res."</table>";
   }
 
-
-
 }
-
-
- ?>
